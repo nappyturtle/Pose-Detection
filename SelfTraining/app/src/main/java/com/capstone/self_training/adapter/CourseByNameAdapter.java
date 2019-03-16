25f16d24a01a353e998d789fa449f69c37a48a63
Phat Le Thinh <41041496+PhatLT1723@users.noreply.github.com>
2019-03-16 20:27:44 +0700

    layout item course in spinner course of trainer upload video

--- SelfTraining/app/src/main/java/com/capstone/self_training/adapter/CourseByNameAdapter.java
+++ SelfTraining/app/src/main/java/com/capstone/self_training/adapter/CourseByNameAdapter.java
@@ -1,5 +1,6 @@
 package com.capstone.self_training.adapter;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,10 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.capstone.self_training.R;
 import com.capstone.self_training.model.Course;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -30,14 +33,30 @@ public class CourseByNameAdapter extends ArrayAdapter<Course> {
         return initView(position, convertView, parent);
     }
 
-    private View initView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
+    @SuppressLint("ResourceAsColor")
+    private View initView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_course_by_name, parent, false);
         }
         TextView tvCourseName = convertView.findViewById(R.id.tvCourseNAme);
+        ImageView ivCourseThumbnail = convertView.findViewById(R.id.iv_course_by_name);
+        TextView tvCourseItemPrice = convertView.findViewById(R.id.tv_course_item_price);
+
         Course cur = getItem(position);
-        if(cur != null){
-            tvCourseName.setText(cur.getName());
+        if (cur != null) {
+            if (cur.getName().equalsIgnoreCase("miễn phí") || cur.getName().equalsIgnoreCase("mien phi") || cur.getPrice() == 0) {
+                tvCourseName.setText("Miễn phí");
+                //tvCourseName.setTextColor(android.R.color.holo_green_light);
+                tvCourseName.setTextColor(R.color.holo_green_light);
+                tvCourseName.setGravity(View.TEXT_ALIGNMENT_TEXT_START);
+                tvCourseItemPrice.setVisibility(View.INVISIBLE);
+                ivCourseThumbnail.setVisibility(View.INVISIBLE);
+            } else {
+                tvCourseName.setText(cur.getName());
+                tvCourseItemPrice.setText(cur.getPrice() + ".000 đ");
+                Picasso.get().load(cur.getThumbnail()).fit().into(ivCourseThumbnail);
+            }
+
         }
         return convertView;
     }
