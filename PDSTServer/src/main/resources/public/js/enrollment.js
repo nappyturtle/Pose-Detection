843df8e88a9e99c356ab76aab04a5db89c7bd52a
Phat Le Thinh <41041496+PhatLT1723@users.noreply.github.com>
2019-04-11 20:50:11 +0700

    enrollment js file

---
+++ PDSTServer/src/main/resources/public/js/enrollment.js
@@ -0,0 +1,234 @@
+$(document).ready(function () {
+    var url_string = window.location.href;
+    var url = new URL(url_string);
+    var accountId = url.searchParams.get("accountId");
+
+    currentStaff = JSON.parse(localStorage.getItem("staff"));
+    if (currentStaff != undefined && currentStaff != null) {
+        $("#btn-sign-out").click(function () {
+            localStorage.removeItem("staff");
+            window.location.href = "../../login.html";
+        })
+        if (currentStaff.roleId == 1) {
+            $("#li-course-url").hide();
+            $("#li-video-url").hide();
+            $("#course-box-info").hide();
+            $("#video-box-info").hide();
+            $("#li-category-url").hide();
+        }
+        if (currentStaff.roleId == 1 || currentStaff.roleId == 2) {
+            console.log("init..............: " + currentStaff);
+            $("#navbar-nav-imgUrl").attr('src', currentStaff.imgUrl);
+            $("#navbar-nav-username").html(currentStaff.username);
+            $("#pull-lelf-user-img").attr('src', currentStaff.imgUrl);
+            $("#pull-lelf-username").html(currentStaff.username);
+            $("#dropdown-menu-imgUrl").attr('src', currentStaff.imgUrl);
+            $("#dropdown-menu-username").html(currentStaff.username);
+            if (currentStaff.roleId == 1) {
+                $("#dropdown-menu-role").html("Admin");
+            } else {
+                $("#dropdown-menu-role").html("Nhân viên");
+            }
+
+            $.ajax({
+                url: "/account/update/" + currentStaff.id,
+                type: "GET",
+                headers: {
+                    "content-type": "application/json; charset=UTF-8"
+                },
+                dataType: "json",
+                success: function (res) {
+                    if (res.fullname != null) {
+                        $(".navbar-username").html(res.fullname);
+                    } else {
+                        $(".navbar-username").html(res.username);
+                    }
+                }
+            })
+
+            //init entollment data
+            $.ajax({
+                url: "/enrollment/enrollmentstats",
+                type: "GET",
+                headers: {
+                    "content-type": "application/json; charset=UTF-8",
+                    "Authorization": currentStaff.token
+                },
+                dataType: "json",
+                success: function (res) {
+                    console.log(res);
+                    dataSrc = $('#tblEnrollment').DataTable({
+                        paging: true,
+                        lengthChange: true,
+                        searching: true,
+                        ordering: true,
+                        info: true,
+                        //autoWidth: true,
+                        data: res
+                        ,
+                        columns: [
+
+                            {
+                                data: null,
+                                sortable: false,
+                                width: 30,
+                                orderable: false,
+                                className: 'row-index'
+                            },
+                            {data: "username"},
+                            {data: "coursename"},
+                            {
+                                data: "enrollment.price",
+                                render: function (data) {
+                                    data = '<span>' + data + ".000" + '</span>'
+                                    return data;
+                                }
+                            },
+                            {
+                                data: "enrollment.id",
+                                searchable: false,
+                                orderable: false,
+                                className: "text-center",
+                                render: function (data, type, row) {
+                                    if (type === 'display') {
+                                        /*data = '<i class="fa fa-fw fa-eye btn-get-details" value="' + ("btnCourse") + data + '" style="padding-left: 32%"></i>';*/
+                                        /*data = '<button type="button" class="btn btn-info btn-get-details" value="' + ("btnCourse") + data + '">Xem chi tiết</button>';*/
+                                        /*data = '<i class="fa fa-fw fa-eye" id="' + data + '" ></i>';*/
+                                        data='<span></span>'
+                                    }
+                                    return data;
+                                }
+                            }
+                        ]
+                        ,
+                        buttons: [
+                            {extend: "edit"},
+                        ],
+                        select: {
+                            style: 'os',
+                            selector: 'td:first-child'
+                        },
+                        rowCallback: function (row, data, index) {
+                            $('.row-index', row).html(index + 1);
+                        }
+                    });
+                }
+            })
+
+            function update($account) {
+                var today = new Date();
+                var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+                var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+                var dateTime = date + ' ' + time;
+                var isMale = false;
+                console.log(dateTime);
+                $account.updatedTime = dateTime;
+                $account.email = $("#input-detail-profile-email").val();
+                $account.phone = $("#input-detail-profile-phone").val();
+                $account.address = $("#input-detail-profile-address").val();
+                $account.status = $("#select-detail-profile-status option:selected").val();
+                $account.gender = $("#select-detail-profile-gender option:selected").val();
+
+                /*var rdGender = $("input[name='r4-gender']").iCheck('update')[0].checked;
+                var gender = "";
+                if (rdGender) {
+                    gender = "male"
+                } else {
+                    gender = "female";
+                }
+                $account.gender = gender;*/
+
+                /*var rdStatus = $("input[name='r3-status']").iCheck('update')[0].checked;
+                var status = "";
+                if (rdStatus) {
+                    status = "active"
+                } else {
+                    status = "inactive";
+                }
+                $account.status = status;
+                console.log($account);*/
+
+                $.ajax({
+                    url: "/account/updateAccount",
+                    type: "POST",
+                    data: JSON.stringify($account),
+                    headers: {
+                        "content-type": "application/json; charset=UTF-8"
+                    },
+                    dataType: "json",
+                    success: function (res) {
+                        console.log(res)
+                        if (res != null) {
+                            if (res == true) {
+                                $("#mUpdateInfo").html("Cập nhật thành công!");
+                                // $("#btn-save-change").hide();
+                                setTimeout(function () {
+                                    location.reload();
+                                }, 1000);
+                                //dismissDialog();
+                                //VuVG 2/4/2019
+                                setTimeout(function () {
+                                    $('#modal-info').modal('hide');
+                                    $("#btn-save-change").hide();
+                                    location.reload();
+                                }, 2000);
+                            } else {
+                                $("#mUpdateInfo").html("Cập nhật thất bại!");
+                                // $("#btn-save-change").hide();
+                                //dismissDialog();
+                                //VuVG 2/4/2019
+                                setTimeout(function () {
+                                    $('#modal-info').modal('hide');
+                                    $("#btn-save-change").hide();
+                                    location.reload();
+                                }, 2000);
+                            }
+                        } else {
+                            console.log("cập nhật thất bại");
+                        }
+                    }
+                })
+            }
+
+            function validData() {
+
+                /*if ($("#input-detail-profile-fullname").val().toString().trim() == "") {
+                    $("#mUpdateInfo").html("Lỗi: Tên không đươc bỏ trống!");
+                    $("#btn-save-change").hide();
+                    return false;
+                }*/
+                console.log($("#input-detail-profile-phone").val().toString().trim().length)
+                var email = $("#input-detail-profile-email").val();
+                if ($("#input-detail-profile-email").val().toString() == "") {
+                    $("#mUpdateInfo").html("Lỗi: Email không được bỏ trống!");
+                    $("#btn-save-change").hide();
+                    return false;
+                } else if (!validateEmail(email)) {
+                    $("#mUpdateInfo").html("Lỗi: Email không đúng định dạng!");
+                    $("#btn-save-change").hide();
+                    return false;
+                }
+
+                if ($("#input-detail-profile-phone").val() == "") {
+                    $("#mUpdateInfo").html("Lỗi: Số diện thoại không được bỏ trống!");
+                    $("#btn-save-change").hide();
+                    return false;
+                } else if ($("#input-detail-profile-phone").val().toString().trim().length < 10 || $("#input-detail-profile-phone").val().toString().trim().length > 15) {
+                    $("#mUpdateInfo").html("Lỗi: Số diện thoại không đúng dịnh dạng!");
+                    $("#btn-save-change").hide();
+                    return false;
+                }
+                return true;
+            }
+
+            function validateEmail(email) {
+                var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                return re.test(email);
+            }
+        } else {
+            window.location.href = "../403.html";
+        }
+    } else {
+        window.location.href = "../403.html";
+    }
+})
